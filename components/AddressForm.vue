bfe5ee5ad3b76918a84b7aa4cb782e58e34a86e2
iamfeanyluch <ifeanyioluchi7@gmail.com>
2023-08-29 05:43:36 +0100

    Added a font

--- components/AddressForm.vue
+++ components/AddressForm.vue
@@ -1,54 +1,39 @@
 <template>
     <form class="form">
-        <div class="form-input">
-            <label>Country:</label>
-            <input v-model="formData.country" />
-        </div>
-        <div class="form-input">
-            <label>State:</label>
-            <input v-model="formData.state" />
-        </div>
-        <div class="form-input">
-            <label>City:</label>
-            <input v-model="formData.city" />
-        </div>
-        <div class="form-input">
-            <label>Street:</label>
-            <input v-model="formData.street" />
-        </div>
-        <div class="form-input">
-            <label>Zip Code:</label>
-            <input v-model="formData.zipCode" />
-        </div>
+        <form-input label="Country" :value="formData.country" />
+        <form-input label="State" :value="formData.state" />
+        <form-input label="City" :value="formData.city" />
+        <form-input label="Street" :value="formData.street" />
+        <form-input label="Zip Code" :value="formData.zipCode" />
     </form>
 </template>
   
 <script>
+import FormInput from "@/components/FormInput.vue"; // Adjust the path accordingly
+
 export default {
     name: "AddressForm",
+    components: {
+        FormInput,
+    },
     props: {
-        formData: Object,
+        value: Object,
+    },
+    computed: {
+        formData: {
+            get() {
+                console.log('formData getter:', this.value);
+                return this.value;
+            },
+            set(formData) {
+                console.log('formData setter:', formData);
+                this.$emit('input', formData);
+            },
+        },
+
     },
 };
 </script>
   
-<style>
-.form-input {
-    display: flex;
-    flex-direction: column;
-    margin: 10px 0;
-}
-
-label {
-    margin: 5px 0;
-}
-
-input {
-    height: 30px;
-    padding: 0 5px;
-    outline: none;
-    border: 1px solid rgb(196, 195, 195);
-    border-radius: 5px;
-}
-</style>
+<style></style>
   
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <div class="flex">
-      <address-form :form-data="formData" />
+      <address-form :value="formData" />
       <map-component :zoom="zoom" :user-location="userLocation" :geojson="geojson" :geojson-options="geojsonOptions" />
     </div>
   </div>
@@ -19,16 +19,14 @@ export default {
   data() {
     return {
       zoom: 3,
-      userLocation: [47.41322, -1.219482], // Default location
+      userLocation: [47.41322, -1.219482],
       geojson: {
         type: "FeatureCollection",
         features: [
-          // ...
         ],
       },
       geojsonOptions: {
         pointToLayer: this.createCircleMarker,
-        // Other options
       },
       formData: {
         country: "",
@@ -43,11 +41,11 @@ export default {
     const { circleMarker } = await import("leaflet/dist/leaflet-src.esm");
     this.geojsonOptions.pointToLayer = (feature, latLng) =>
       circleMarker(latLng, { radius: 8 });
-    this.getUserLocation(); // Get user's location on mount
+    this.getUserLocation();
   },
   methods: {
     createCircleMarker(feature, latLng) {
-      const { circleMarker } = L; // Import circleMarker from Leaflet
+      const { circleMarker } = L;
       return circleMarker(latLng, { radius: 8 });
     },
     getUserLocation() {
@@ -56,7 +54,6 @@ export default {
           const { latitude, longitude } = position.coords;
           this.userLocation = [latitude, longitude];
 
-          // Assuming you have a function to fetch address details from coordinates
           this.fetchAddressFromCoordinates(latitude, longitude)
             .then((address) => {
               this.formData.country = address.country;
@@ -74,26 +71,27 @@ export default {
       }
     },
     async fetchAddressFromCoordinates(latitude, longitude) {
-      // Use an API or service to fetch address details based on coordinates
-      // For example, you can use the OpenStreetMap Nominatim API as shown before.
       const response = await fetch(
         `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
       );
       const data = await response.json();
 
-      // Extract the address components you need
+      console.log("Fetched address data:", data);
+
       const address = {
         country: data.address.country,
         state: data.address.state,
-        city: data.address.city,
-        street: data.address.road, // Use road property for street
+        city: data.address.county,
+        street: data.address.road,
         zipCode: data.address.postcode,
       };
 
+      console.log("Extrated Address", address)
+
       return address;
     },
+
   },
-  // Other methods and lifecycle hooks...
 };
 </script>
 
@@ -103,7 +101,9 @@ body {
   margin: 0;
   box-sizing: border-box;
   background: rgb(238, 238, 238);
+  font-family: 'Inter', sans-serif;
 }
+
 .flex {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
